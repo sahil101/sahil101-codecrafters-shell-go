efb9e66cf772dd7f692f9e579e2a7ccba8e4cd1e
Sahil.Agarwal <sahil.agarwal@SahilAgarwals-MacBook-Pro.local>
2025-02-24 23:27:36 +0530

    adding type command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -26,20 +26,22 @@ func main() {
 		}
 
 		switch params[0] {
+		case "type":
+			switch params[1] {
+			case "echo":
+				fmt.Println("echo is a shell builtin")
+			case "exit":
+				fmt.Println("exit is a shell builtin")
+			default:
+				fmt.Println(params[1] + ": not found")
+			}
 		case "echo":
 			fmt.Println(strings.Join(params[1:], " "))
 		case "exit":
-			if params[1] == "0" {
-				os.Exit(0)
-			}
-
+			os.Exit(0)
 		default:
 			fmt.Println(command + ": command not found")
 		}
-
-		// if command == "\n" {
-		// 	break
-		// }
 
 	}
 
