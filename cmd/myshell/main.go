5341d925322ba6fdf35b5338abbba84d02463592
Sahil.Agarwal <sahil.agarwal@SahilAgarwals-MacBook-Pro.local>
2025-02-24 23:17:01 +0530

    adding echo command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -16,14 +17,22 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 		// Wait for user input
 		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		command = strings.Trim(command, "\n\r")
+		params := strings.Fields(command)
+
 		if err != nil {
 			fmt.Fprintln(os.Stderr, "Error reading input:", err)
 			os.Exit(1)
 		}
 
-		switch command {
-		case "exit 0\n":
-			os.Exit(0)
+		switch params[0] {
+		case "echo":
+			fmt.Println(strings.Join(params[1:], " "))
+		case "exit":
+			if params[1] == "0" {
+				os.Exit(0)
+			}
+
 		default:
 			fmt.Println(command[:len(command)-1] + ": command not found")
 		}
