64eb80122a08936861d13673503d2238ec58fc2f
Sahil.Agarwal <sahil.agarwal@SahilAgarwals-MacBook-Pro.local>
2025-02-24 18:40:30 +0530

    fix

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -15,5 +15,9 @@ func main() {
 
 	// Wait for user input
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Error reading input:", err)
+		os.Exit(1)
+	}
 	fmt.Println(command[:len(command)-1] + ": command not found")
 }
